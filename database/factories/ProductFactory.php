3267cdc79ac21192408ebdc1f5b1442ffbcfc7f4
willybliss <wandoh-mensah@itconsortiumgh.com>
2019-10-08 14:04:08 +0000

    Created Model -a

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\product;
+use Faker\Generator as Faker;
+
+$factory->define(product::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
